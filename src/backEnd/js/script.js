2d164f782232f0975019d788f4ece3d2bc46a497
Chris Jim <chrisjim316@gmail.com>
2017-09-30 14:01:48 +0100

    Update script.js

--- src/backEnd/js/script.js
+++ src/backEnd/js/script.js
@@ -2,7 +2,7 @@
 $(document).ready( function() {
   $("#start").on("click", function () {
     /* fade in effect */
-    $("#start").load('src/frontEnd/video.html',function(){}).hide().fadeIn();
+
   });
 });
 
