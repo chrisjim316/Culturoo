39c2d6303284a46cdcdef3c51edc9cdaf2354ba9
Ace89 <awaistalib@hotmail.com>
2017-09-30 12:53:12 +0100

    Added test directory

--- src/backEnd/src/server.py
+++ src/backEnd/src/server.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def hello():
     return "hello"
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run()
---
+++ src/backEnd/tests/nltk_test.py
@@ -0,0 +1,10 @@
+import nltk
+
+
+def check_version():
+    print(str(nltk.__version__))
+
+if __name__ == "__main__":
+    check_version()
+
+
