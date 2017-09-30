b28c842724d0a74cb79318797851f660b049e802
Chris Jim <chrisjim316@gmail.com>
2017-09-30 14:24:56 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+$(document).ready( function() {
+  $("#start").on("click", function () {
+    /* fade in effect */
+    $("#start").load('https://chrisjim316.github.io/Video/',function(){}).fadeIn();
+  });
+});
+
